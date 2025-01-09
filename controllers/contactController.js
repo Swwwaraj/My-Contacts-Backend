66a12dd5fd80f14a9142207de77971f3c1a2e1f7
Swwwaraj <zavareswaraj226@gmail.com>
2025-01-10 00:15:30 +0530

    CRUD operation and Connect Mongoose

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,43 +1,74 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 //@Desc Get all contacts
 //@Roure GET /api/contacts
 //@Access Public
-const getContact = (req, res) => {
-    res.status(200).json({ message: "Get all contacts" });
-};
+const getContact = asyncHandler(async(req, res) => {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+});
 
 //@Desc Create New contact
 //@Roure POST /api/contacts
 //@Access Public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async(req, res) => {
     console.log("The request body is :", req.body);
     const {name, email, phone} = req.body;
     if(!name || !email || !phone){
         res.status(400);
         throw new Error ("All fields are mandatory !")
     }
-    res.status(201).json({ message: "Create contact" });
-};
+
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+    });
+    res.status(201).json(contact);
+});
 
 //@Desc Get contact
 //@Roure GET /api/contacts/:id
 //@Access Public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async(req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact){
+        res.status(404);
+        throw new Error ("Contact not found");
+    }
     res.status(200).json({ message: `Get contact for ${req.params.id}` }); // Use template literal and req.params.id
-};
+});
 
 //@Desc update the contact
 //@Roure UPDATE /api/contacts/:id
 //@Access Public
-const updateContact = (req, res) => {
-    res.status(200).json({ message: `Update contact for ${req.params.id}` }); // Corrected template literal
-};
+const updateContact = asyncHandler(async(req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact){
+        res.status(404);
+        throw new Error ("Contact not found");
+    };
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new : true}
+        
+    );
+    res.status(200).json(updateContact); // Corrected template literal
+});
 
 //@Desc Delete the contact
 //@Roure DELETE /api/contacts/:id
 //@Access Public
-const deleteContact = (req, res) => {
-    res.status(200).json({ message: `Delete contact for ${req.params.id}` }); // Corrected template literal
-};
+const deleteContact = asyncHandler(async(req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact){
+        res.status(404);
+        throw new Error ("Contact not found");
+    }
+    await contact.remove();
+    res.status(200).json(contact); // Corrected template literal
+});
 
 module.exports = { 
     getContacts,
